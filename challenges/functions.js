d2de8a76f89f515927eddc7b381cd0f221ad3ac1
Blake Mitchell <mblakeandrew@gmail.com>
2019-05-10 10:25:46 -0700

    implemented a second imperfect solution to the callbacks problem, which requires reversal of the function calls rather than mutlpiple HOFs

--- challenges/functions.js
+++ challenges/functions.js
@@ -13,7 +13,8 @@ sumNums(11, 4, summer);
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+//solution 1
+/*
 function consume(a, b, callb){
   callb(a + b);
 }
@@ -23,11 +24,6 @@ function consume1(a, b, callb){
 function consume2(a, b, callb){
   callb(`Hello ${a} ${b}, nice to meet you!`);
 }
-/* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
 function add(sum) {
   console.log(sum);
   return sum;
@@ -40,12 +36,54 @@ function greeting(phrase) {
   console.log(phrase);
   return phrase;
 }
+*/
+
+//solution 2
+function add(a, b, cb){
+  cb(a + b);
+}
+function multiply(a, b, cb) {
+  cb(a* b);
+}
+function greeting(a, b, cb) {
+  cb(`Hello ${a} ${b}, nice to meet you!`);
+}
+function consume(pass){
+  console.log(pass);
+  return pass;
+}
+/* Step 2: Create several functions to callback with consume();
+  * Create a function named add that returns the sum of two numbers
+  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+*/
+/*
+function add(sum) {
+  console.log(sum);
+  return sum;
+}
+function multiply(product) {
+  console.log(product);
+  return product;
+}
+function greeting(phrase) {
+  console.log(phrase);
+  return phrase;
+}
+*/
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
+//solution 1
+/*
 consume(2,2,add); // 4
 consume1(10,16,multiply); // 160
 consume2("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+*/
 
+//solution 2
+add(2,2,consume); // 4
+multiply(10,16, consume); // 160
+greeting("Mary","Poppins", consume); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ==== 
 
